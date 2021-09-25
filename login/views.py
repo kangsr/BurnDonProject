5dae6e8f45b97800e91c30134cb4c3101697e02d
NyeonK <skdus728@gmail.com>
2021-09-25 14:30:37 +0900

    edit main page

--- login/views.py
+++ login/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render, redirect
-
-from django.contrib.auth import logout
+from django.contrib import auth
+from django.contrib.auth import authenticate, login
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
-def login(request):
-    return render(request, "login.html")
 
-    
-def logout_view(request):
-    logout(request)
-    return redirect('home')
+def login(req):
+    if req.method == "POST":
+        form = AuthenticationForm(request=req, data=req.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request=req, username=username, password=password)
+            if user is not None:
+                auth.login(req, user)
+                return redirect("home")
+            else:
+                return HttpResponse('Login failed. Try again.')
+    else:
+        form = AuthenticationForm()
+
+    return render(req, "login.html", {"form": form})
+
+
+def logout_view(req):
+    logout(req)
+    return redirect("home")
--- main/forms.py
+++ main/forms.py
@@ -1,10 +1,17 @@
 from django import forms
+from django.forms import ModelForm, NumberInput
 from .models import UserData
 
 class InputForm(forms.ModelForm):
     class Meta:
         model = UserData
         fields = ['goldMoney', 'currentMoney', 'salary', 'age', 'beforeExpense', 'afterExpense']
+        
+        # widgets = {
+        #     'goldMoney' : NumberInput(attrs={
+        #         'class': "",
+        #     })
+        # }
 
 class InputForm2(forms.ModelForm):
     class Meta:
